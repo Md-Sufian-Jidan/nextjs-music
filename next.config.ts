34d6c7e9407eeaf6b32ca1ef9ad0033528d44e4b
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-06-02 14:09:52 +0600

    instructors section added

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com']
+  },
+}
 export default nextConfig;
